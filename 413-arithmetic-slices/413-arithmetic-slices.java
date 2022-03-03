df31818ce2f45f71ac93355bbe36c0fe6aaf8832
NUPUR <55540307+Nupur-me@users.noreply.github.com>
2022-03-04 01:27:18 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (46.33%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -1,23 +1,24 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
         
-        if(nums.length<2){
+        if(nums.length < 2){
             return 0;
         }
         
-           int slices = 0;
-           int prev = 0;
-    
-	           for (int i = 2; i < nums.length; i++){
-                   if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
-	                    prev = prev + 1;
-	                    slices = slices + prev;
-}                  else {
-	               prev = 0;
-                   }
-               }
-		
-	return slices;      
+        int slice = 0;
+        int prev = 0;
         
+        for(int i = 2;i<nums.length;i++){
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]){
+                prev = prev + 1;
+                slice = slice + prev;
+            }
+            
+            else{
+                prev = 0;
+            } 
+        }
+        
+        return slice;
     }
 }
