a3584363f5f96278d811e944fb6eb7b598b64b8c
NUPUR <55540307+Nupur-me@users.noreply.github.com>
2022-04-22 20:26:17 +0530

    Time: N/A, Memory: N/A',
      },
      questionId: '101',
      submissionId: '685458038',
      sessionId: '3438893',
      getLangDisplay: 'java',
      submissionCode: '/**\u000A * Definition for a binary tree node.\u000A * public class TreeNode {\u000A *     int val\u003B\u000A *     TreeNode left\u003B\u000A *     TreeNode right\u003B\u000A *     TreeNode() {}\u000A *     TreeNode(int val) { this.val \u003D val\u003B }\u000A *     TreeNode(int val, TreeNode left, TreeNode right) {\u000A *         this.val \u003D val\u003B\u000A *         this.left \u003D left\u003B\u000A *         this.right \u003D right\u003B\u000A *     }\u000A * }\u000A */\u000Aclass Solution{\u000Apublic boolean isSymmetric(TreeNode root) {\u000A    \u000A    if(root \u003D\u003D null) return true\u003B\u000A    \u000A    Stack\u003CTreeNode\u003E output \u003D new Stack\u003C\u003E()\u003B\u000A    output.push(root.left)\u003B\u000A    output.push(root.right)\u003B\u000A    \u000A    while(!output.empty()){\u000A    TreeNode n1 \u003D output.pop()\u003B\u000A    TreeNode n2 \u003D output.pop()\u003B\u000A    \u000A    if (n1 \u003D\u003D null \u0026\u0026 n2 \u003D\u003D null) continue\u003B\u000A    if(n1 \u003D\u003D null || n2 \u003D\u003D null || n1.val!\u003Dn2.val) return false\u003B\u000A    \u000A    output.push(n1.left)\u003B\u000A    output.push(n2.right)\u003B\u000A    output.push(n1.right)\u003B\u000A    output.push(n2.left)\u003B\u000A    }\u000A    return true\u003B\u000A    \u000A}\u000A    \u000A}',
      editCodeUrl: '/explore/item/627',
      checkUrl: '/submissions/detail/685458038/check/',
      runtimeDistributionFormatted: '{\u0022lang\u0022: \u0022java\u0022, \u0022distribution\u0022: [[\u00220\u0022, 47.8575], [\u00221\u0022, 40.9339]]}',
      memoryDistributionFormatted: '{\u0022lang\u0022: \u0022java\u0022, \u0022distribution\u0022: [[\u002236200\u0022, 0.0057], [\u002237200\u0022, 0.0057], [\u002239600\u0022, 0.0229], [\u002239700\u0022, 0.0229], [\u002239800\u0022, 0.0459], [\u002239900\u0022, 0.3212], [\u002240000\u0022, 1.0497], [\u002240100\u0022, 1.5832], [\u002240200\u0022, 2.6903], [\u002240300\u0022, 3.2008], [\u002240400\u0022, 3.8892], [\u002240500\u0022, 3.0746], [\u002240600\u0022, 2.6616], [\u002240700\u0022, 2.7362], [\u002240800\u0022, 1.6004], [\u002240900\u0022, 0.8547], [\u002241000\u0022, 0.1147], [\u002241100\u0022, 0.109], [\u002241200\u0022, 0.1033], [\u002241300\u0022, 0.2122], [\u002241400\u0022, 0.3499], [\u002241500\u0022, 0.6769], [\u002241600\u0022, 0.9407], [\u002241700\u0022, 1.4169], [\u002241800\u0022, 1.9962], [\u002241900\u0022, 2.7649], [\u002242000\u0022, 3.9006], [\u002242100\u0022, 5.0594], [\u002242200\u0022, 5.6502], [\u002242300\u0022, 6.5278], [\u002242400\u0022, 6.6885], [\u002242500\u0022, 6.3615], [\u002242600\u0022, 6.3443], [\u002242700\u0022, 5.5756], [\u002242800\u0022, 5.0708], [\u002242900\u0022, 4.3595], [\u002243000\u0022, 3.7745], [\u002243100\u0022, 3.0517]]}',
      langs: [
        
      ],
      runtime: 'None',
      memory: 'None',
      enableMemoryDistribution: 'True',
      nonSufficientMsg: 'Sorry. We do not have enough accepted submissions to show distribution chart. - LeetHub

--- symmetric-tree/symmetric-tree.java
+++ symmetric-tree/symmetric-tree.java
@@ -15,18 +15,27 @@
  */
 class Solution{
 public boolean isSymmetric(TreeNode root) {
-    if (root == null) return true;
-    Stack<TreeNode> stack = new Stack<>();
-    stack.push(root.left);
-    stack.push(root.right);
-    while (!stack.empty()) {
-        TreeNode n1 = stack.pop(), n2 = stack.pop();
-        if (n1 == null && n2 == null) continue;
-        if (n1 == null || n2 == null || n1.val != n2.val) return false;
-        stack.push(n1.left);
-        stack.push(n2.right);
-        stack.push(n1.right);
-        stack.push(n2.left);
+    
+    if(root == null) return true;
+    
+    Stack<TreeNode> output = new Stack<>();
+    output.push(root.left);
+    output.push(root.right);
+    
+    while(!output.empty()){
+    TreeNode n1 = output.pop();
+    TreeNode n2 = output.pop();
+    
+    if (n1 == null && n2 == null) continue;
+    if(n1 == null || n2 == null || n1.val!=n2.val) return false;
+    
+    output.push(n1.left);
+    output.push(n2.right);
+    output.push(n1.right);
+    output.push(n2.left);
     }
     return true;
-}}
+    
+}
+    
+}
