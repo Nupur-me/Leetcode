8fca5a4d89fa99c157e2cd3c7b709d63be28e680
NUPUR <55540307+Nupur-me@users.noreply.github.com>
2022-04-22 18:10:55 +0530

    Time: 2 ms, Memory: 42.2 MB - LeetHub

--- symmetric-tree/symmetric-tree.java
+++ symmetric-tree/symmetric-tree.java
@@ -13,17 +13,20 @@
  *     }
  * }
  */
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-       
-        return root == null || helper(root.left,root.right);
+class Solution{
+public boolean isSymmetric(TreeNode root) {
+    if (root == null) return true;
+    Stack<TreeNode> stack = new Stack<>();
+    stack.push(root.left);
+    stack.push(root.right);
+    while (!stack.empty()) {
+        TreeNode n1 = stack.pop(), n2 = stack.pop();
+        if (n1 == null && n2 == null) continue;
+        if (n1 == null || n2 == null || n1.val != n2.val) return false;
+        stack.push(n1.left);
+        stack.push(n2.right);
+        stack.push(n1.right);
+        stack.push(n2.left);
     }
-    
-    public boolean helper(TreeNode left, TreeNode right){
-        
-        if(left==null || right == null) return left == right;
-        if(left.val != right.val) return false;
-        
-        return helper(left.left, right.right) && helper(left.right , right.left);
-    }
-}
+    return true;
+}}
