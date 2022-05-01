249fbc5e9431ed36fb7f313bef9fc8e6da5744e9
NUPUR <55540307+Nupur-me@users.noreply.github.com>
2022-05-01 12:23:36 +0530

    Time: 2 ms, Memory: 48.1 MB - LeetHub

--- fizz-buzz/fizz-buzz.java
+++ fizz-buzz/fizz-buzz.java
@@ -2,15 +2,23 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         
         ArrayList<String> ans = new ArrayList<String>(n);
-        for(int i = 1; i<=n;i++){
-        if(i%3==0 && i%5==0){
+        for(int i = 1, fizz = 0, buzz = 0; i<=n; i++){
+            fizz++;
+            buzz++;
+        if(fizz==3 && buzz == 5){
             ans.add("FizzBuzz");
+            fizz = 0;
+            buzz = 0;
         }
-        else if(i%3==0){
+        else if(fizz == 3){
+            fizz = 0;
             ans.add("Fizz");
+            
         }
-        else if(i%5==0){
+        else if(buzz == 5){
+             buzz = 0;
             ans.add("Buzz");
+           
         }
         else{
             ans.add(Integer.toString(i));
