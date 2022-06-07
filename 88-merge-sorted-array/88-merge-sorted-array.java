d6c1b41e80e177021514ecc9d4c6fb597bd49617
NUPUR <55540307+Nupur-me@users.noreply.github.com>
2022-06-07 23:23:45 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (99.46%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -5,14 +5,12 @@ class Solution {
         int j = n-1;
         int k = m+n-1;
         
-        while(i>=0 && j>=0){
+        while(i>=0 && j >=0){
             
-            if(nums1[i]<nums2[j]){
-                
-                nums1[k] = nums2[j];
+            if(nums1[i] < nums2[j]){
+             nums1[k] = nums2[j];
                 k--;
                 j--;
-                
             }
             
             else{
@@ -21,17 +19,17 @@ class Solution {
                 i--;
             }
         }
-            
-            while(i>=0){
-                nums1[k] = nums1[i];
-                k--;
-                i--;
-            }
-            
-            while(j>=0){
-                nums1[k] = nums2[j];
-                k--;
-                j--;
-            }
+        
+        while(i>=0){
+            nums1[k] = nums1[i];
+            k--;
+            i--;
+        }
+        
+        while(j>=0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
     }
 }
