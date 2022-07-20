a5775042eaa4e4dd88e5fb574e83b4467525a07b
NUPUR <55540307+Nupur-me@users.noreply.github.com>
2022-07-20 15:53:53 +0530

    Time: 15 ms (45.70%), Space: 55 MB (65.45%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,44 +1,25 @@
 class Solution {
    public int[][] merge(int[][] intervals) {
        
-   	// sort our intervals 
-     Arrays.sort(intervals, (a1,a2)->a1[0]-a2[0]);
+     Arrays.sort(intervals, (a1,a2)-> Integer.compare(a1[0],a2[0]));
   
-       ArrayList<int[]> ans  =  new ArrayList<>();
+     ArrayList<int[]> list  =  new ArrayList<>();
        
-   	// intial range 
-       int start  =  intervals[0][0];
-       int end =  intervals[0][1]; 
-       
-       int  i =1;
-       while(i<intervals.length){
-           int s = intervals[i][0];
-           int e = intervals[i][1];
-           
-           // next interval start is smaller than prev end and array is sorted
-           // so these two internal can merge
-           
-           if( s<=end  ) { 
-               // so merge both intervals 
-               end =  Math.max(end,e);
-           }
-           else{ 
-               
-               // if merge not possible , then insert prev interval into list
-               
-               ans.add(new int[]{start,end});
-               start = s;
-               end =  e;                
-           }
-          i++;
-       }
-       
-      ans.add(new int[] {start,end});
-       
-   	  int arr[][] =  new int[ans.size()][]; 
-   	  return ans.toArray(new int[0][]);
-       
-       
+     for(int[] interval: intervals){
+         if(list.size() == 0){
+             list.add(interval);
+         }
+         else{
+             int prevInterval[] = list.get(list.size() - 1);
+             if(interval[0] <= prevInterval[1]){
+                 prevInterval[1] = Math.max(prevInterval[1],interval[1]);
+             
+         }else{
+             list.add(interval);
+         }
+     }
+     
        
    }
-}
+       return list.toArray(new int[list.size()][]);
+}}
